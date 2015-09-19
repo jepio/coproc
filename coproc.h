d3c8c45a7523930eff92c3c95ffab59e2241e25b
Jeremi Piotrowski <jeremi.piotrowski@gmail.com>
2015-09-19 19:57:53 +0200

    coproc: apply clang-format

--- coproc.h
+++ coproc.h
@@ -1,15 +1,15 @@
 /******************************************************************************
  *
- * © Copyright 2007,2008,2009 Codefidence Ltd. 
+ * © Copyright 2007,2008,2009 Codefidence Ltd.
  * All rights reserved unless explictly stated otherwise.
  *
  * This file is part of CoProc.
  *
  * CoProc is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as 
- * published by the Free Software Foundation, either version 3 of 
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
- * 
+ *
  * CoProc is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -39,35 +39,35 @@
 
 struct coproc_attributes {
 
-	rlim_t  address_space_size; 	/* The maximum size of the process 
-						address space in bytes */
-	rlim_t  core_file_size;		/* Maximum size of core file */
-	rlim_t  cpu_time;		/* CPU time limit in seconds */
-	rlim_t  stack_size;		/* The maximum size of the process 
-						stack, in bytes */
-	int 	  scheduling_policy; 	/* Scheduling policy. */
-	int 	  scheduling_param;	/* Scheduling priority or nice level */
-	cpu_set_t cpu_affinity_mask;	/* The CPU mask of the co-process */
+	rlim_t address_space_size;   /* The maximum size of the process
+					     address space in bytes */
+	rlim_t core_file_size;       /* Maximum size of core file */
+	rlim_t cpu_time;	     /* CPU time limit in seconds */
+	rlim_t stack_size;	   /* The maximum size of the process
+						   stack, in bytes */
+	int scheduling_policy;       /* Scheduling policy. */
+	int scheduling_param;	/* Scheduling priority or nice level */
+	cpu_set_t cpu_affinity_mask; /* The CPU mask of the co-process */
 };
 
-#define	COPROC_NOTIFY_PARENT 	(1)
-#define COPROC_SHARE_FS 	(1<<1)
-#define COPROC_DETACHED         (1<<2)
-#define	COPROC_SHARE_SYSVSEM	(1<<3)
-#define	COPROC_SET_AS_SIZE	(1<<4)
-#define	COPROC_SET_CORE_SIZE	(1<<5)
-#define	COPROC_SET_CPU_TIME	(1<<6)
-#define	COPROC_SET_STACK_SIZE	(1<<6)
-#define	COPROC_SET_SCHED	(1<<7)
-#define	COPROC_SET_CPU_AFFINITY (1<<8)
+#define COPROC_NOTIFY_PARENT (1)
+#define COPROC_SHARE_FS (1 << 1)
+#define COPROC_DETACHED (1 << 2)
+#define COPROC_SHARE_SYSVSEM (1 << 3)
+#define COPROC_SET_AS_SIZE (1 << 4)
+#define COPROC_SET_CORE_SIZE (1 << 5)
+#define COPROC_SET_CPU_TIME (1 << 6)
+#define COPROC_SET_STACK_SIZE (1 << 6)
+#define COPROC_SET_SCHED (1 << 7)
+#define COPROC_SET_CPU_AFFINITY (1 << 8)
 
 /******************************************************************************
  * coproc_init
  *
- * Init coproc library 
+ * Init coproc library
  *
- * Call must be done prior * to the first use of any of the 
- * other coproc library APIs 
+ * Call must be done prior * to the first use of any of the
+ * other coproc library APIs
  *
  * shm_max_size:   the maximum number, in bytes of allocatable shared memory.
  */
@@ -76,8 +76,8 @@ int coproc_init(size_t shm_max_size);
 
 /******************************************************************************
  * coproc_create
- * 
- * Create a new co-process 
+ *
+ * Create a new co-process
  *
  * coproc_name:   a string with the new co-process name.
  *
@@ -85,9 +85,9 @@ int coproc_init(size_t shm_max_size);
  * 			attributes for the newly created co-process or
  * 			NULL for the default attributes.
  *
- * flags:	  a mask of one or more of:	
+ * flags:	  a mask of one or more of:
  *
- * 				COPROC_NOTIFY_PARENT - make the new co-process 
+ * 				COPROC_NOTIFY_PARENT - make the new co-process
  * 				inherit creator parent as opposed to the default
  * 				of making the creator the parent.
  *
@@ -96,21 +96,21 @@ int coproc_init(size_t shm_max_size);
  * 				with creator as opposed of getting a copy
  * 				of the creator resources.
  *
- * 				COPROC_SHARE_PTRACE - trace new co-process 
+ * 				COPROC_SHARE_PTRACE - trace new co-process
  * 				when creator is traced (debugged).
  *
  * 				COPROC_SHARE_SYSVSEM - new co-process will
- * 				share SystemV semaphores undo values as 
+ * 				share SystemV semaphores undo values as
  * 				oppoused to default of getting an empty set.
  *
  * 				COPROC_SET_AS_SIZE - set address space size
- * 				of new co-process according to the limit 
- * 				specified in attributes argument as oppused 
+ * 				of new co-process according to the limit
+ * 				specified in attributes argument as oppused
  * 				to inherting from creator.
  *
  * 				COPROC_SET_CORE_SIZE - set the core size
  * 				of the new co-process according to the limit
- * 				specified in the attributes argument as 
+ * 				specified in the attributes argument as
  * 				oppused of inhgerting from creator.
  *
  * 				COPROC_SET_CPU_TIME - set the maximum cpu
@@ -123,27 +123,27 @@ int coproc_init(size_t shm_max_size);
  * 				limit specified in the attributes argument as
  * 				oppused of inhgerting from creator.
  *
- * 				COPROC_SET_SCHED - set the scheduling policy 
+ * 				COPROC_SET_SCHED - set the scheduling policy
  * 				and priority according to the attributes
  * 				argument oppused of inhgerting from creator.
  *
  * 				COPROC_SET_CPU_AFFINITY - set the CPU affinity
- * 				mask of the new co-process according to the 
- * 				attributes argument oppused of inhgerting from 
+ * 				mask of the new co-process according to the
+ * 				attributes argument oppused of inhgerting from
  * 				creator.
  *
  * start_routine: the address of the function to call in the new co-process.
- * 
+ *
  * arg:		  an argument passed to the start_routine when called.
  */
 
-pid_t coproc_create(char * coproc_name, struct coproc_attributes * attrib, \
-        int flags, int (* start_routine)(void *), void * arg);
+pid_t coproc_create(char *coproc_name, struct coproc_attributes *attrib,
+		    int flags, int (*start_routine)(void *), void *arg);
 
 /******************************************************************************
  * coproc_exit
  *
- * Terminate the program and all associated co-processes 
+ * Terminate the program and all associated co-processes
  */
 
 int coproc_exit(void);
@@ -157,7 +157,7 @@ int coproc_exit(void);
  * 			to allocate.
  */
 
-void * coproc_alloc(size_t size);
+void *coproc_alloc(size_t size);
 
 /******************************************************************************
  * coproc_free
@@ -172,15 +172,15 @@ void coproc_free(void *ptr);
 /******************************************************************************
  * coproc_join
  *
- * Synchronize on the termination of a co-processes and read it's return 
+ * Synchronize on the termination of a co-processes and read it's return
  * status
  *
  * pid:		the process ID of the co-process to join.
  *
- * status:	the address of an integer to which the co-process status 
+ * status:	the address of an integer to which the co-process status
  * 			is copied.
  */
 
-int coproc_join(pid_t pid, int * status);
+int coproc_join(pid_t pid, int *status);
 
 #endif /* _COPROC_H_ */
